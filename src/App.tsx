51936971092d6c0db4195cc22228baa90f98caa0
elias <eliasfaculdade@yahoo.com.br>
2024-07-20 15:12:13 -0300

    novo projeto

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import{Nav} from "./components";
+
+export default function App() {
+    return (
+    <div className="App">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Nav/>
+    </div>
+  );
+
+};
+
+
+
+
