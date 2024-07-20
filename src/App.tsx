173d7e3e354c204eb7aa7d5a98c36a9f635dfa2d
elias <eliasfaculdade@yahoo.com.br>
2024-07-20 19:11:52 -0300

    commit message

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Nav } from "./components"; /*erroao tenatr colocar caminho absoluto !!! olhar */
+
+export default function App() {
+    return (
+    <div className="App">
+    <Nav />
+    </div>
+  );
+};
+
+
+
+
---
+++ src/components/Nav/NavLogo.tsx
@@ -0,0 +1,9 @@
+export function NavLogo() {
+  return(
+    <div>
+    <p className="text-2xl">
+        Schedule<span className="font-bold italic">Mail</span>
+    </p>
+    </div>
+  )
+}
